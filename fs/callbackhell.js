641f4861c487a584b8d8d1bf6685cecdbb7f5f33
RafayMemon01 <rafayrashid200611@gmail.com>
2024-08-29 00:53:35 +0500

    callback hell

--- fs/callbackhell.js
+++ fs/callbackhell.js
@@ -1,8 +1,44 @@
 import fs from 'fs'
 
+// This is how callback hell looks like 
 
 (() =>{
-    console.log(fs)
+
+
+    fs.writeFile(
+        //filename        //text                                                    //callback
+        "hello2.txt", "hello world this file is created with fs writeFile function", () => {
+          console.log("file write done")
+          fs.readFile("hello2.txt", "utf-8", (err, data) => {
+              console.log(err);
+              console.log(data);
+              //again
+              fs.writeFile(
+                //filename        //text                                                    //callback
+                "hello2.txt", "hello world this file is created with fs writeFile function", () => {
+                  console.log("file write done")
+                  fs.readFile("hello2.txt", "utf-8", (err, data) => {
+                      console.log(err);
+                      console.log(data);
+                      //again
+                      fs.writeFile(
+                        //filename        //text                                                    //callback
+                        "hello2.txt", "hello world this file is created with fs writeFile function", () => {
+                          console.log("file write done")
+                          fs.readFile("hello2.txt", "utf-8", (err, data) => {
+                              console.log(err);
+                              console.log(data);
+                              //again
+                          });
+                        }
+                      );
+                  });
+                }
+              );
+          });
+        }
+      );
+
 
   
 })()
